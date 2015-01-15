eca9c96b7b355d3d5351250b276aa6b833ca7459
Jacopo Sinigaglia <jacopo.sinigaglia@gmail.com>
2015-01-15 22:58:50 +0100

    .

--- Assets/Managers/CameraController.cs
+++ Assets/Managers/CameraController.cs
@@ -2,28 +2,37 @@
 using System.Collections.Generic;
 
 public class CameraController : MonoBehaviour {
-    [Range(0, 1)]
-    public float offset;
+    public Camera mainCamera;
+    [Range(0, 2)]
+    public float rotationFocus;
+    [Range(0f, 0.2f)]
+    public float mapCuttingOffset;
     [Range(0, 1)]
     public float camSpeed;
-    public float upY, lowY;
-    public float upRotation, lowRotation;
+    public float upYBoundary, lowYBoundary;
+    public float upRotationBoundary, lowRotationBoundary;
     float upX, upZ, lowX, lowZ;
-    public Camera mainCamera;
-
+    [Range(1, 2)]
+    public float exponentialScroll;
+    [Range((float)0.9, (float)0.97)]
+    public float exponentialDecay;
+    [Range(2, 20)]
+    public float exponentialCap;
     private float expFactor = 1;
+    private float oldScroll = 0;
+    private float timeSinceDecay;
  // Use this for initialization
 	void Start () {
         int sizeX = TerrainManager.sizeX;
         int sizeZ = TerrainManager.sizeZ;
-        lowX = sizeX * offset;
-        lowZ = sizeZ * offset;
-        upX = sizeX * (1-offset);
-        upZ = sizeZ * (1-offset);
+        lowX = sizeX * mapCuttingOffset;
+        lowZ = sizeZ * mapCuttingOffset;
+        upX = sizeX * (1-mapCuttingOffset);
+        upZ = sizeZ * (1-mapCuttingOffset);
 
         //upY = 10f;
         //lowY = 0f;
-        transform.position = new Vector3(sizeX * offset, 0, sizeX * offset);
+        transform.position = new Vector3(5 + sizeX * mapCuttingOffset , 0, 5+ sizeZ * mapCuttingOffset);
 	}
 	
 	// Update is called once per frame
@@ -47,44 +56,71 @@ public class CameraController : MonoBehaviour {
         }
         if (Input.GetButton("RotateLeft"))
         {
-
-            if (tryTranslate(-0.3f * 60f * Time.deltaTime, 0, 0))
-            {
-                transform.Rotate(0, 60f * Time.deltaTime, 0);
-            }
+            transform.Translate(-0.3f * 60f * rotationFocus * mainCamera.transform.position.y * Time.deltaTime, 0, 0);
+            transform.Rotate(0, 60f * Time.deltaTime, 0);
+            
         }
         if (Input.GetButton("RotateRight"))
         {
-
-            if (tryTranslate(0.3f * 60f * Time.deltaTime, 0, 0))
-            {
-                transform.Rotate(0, -60f * Time.deltaTime, 0);
-            }
+            transform.Translate(0.3f * 60f * rotationFocus * mainCamera.transform.position.y * Time.deltaTime, 0, 0);
+            transform.Rotate(0, -60f * Time.deltaTime, 0);   
         }
+
+
+        calcVerticalScroll();
+
+
+
+    }
+    private void calcVerticalScroll()
+    {
+
+        float currentScroll = Input.GetAxis("Mouse ScrollWheel");
+        oldScroll = oldScroll * 0.9f + currentScroll * 0.3f;
+
         if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
-            expFactor = Mathf.Min((float)12.0, expFactor * (float)1.3);
-            tryTranslate(0, 60f *  expFactor * camSpeed * Input.GetAxis("Mouse ScrollWheel"), 0);
 
+            expFactor = Mathf.Min(exponentialCap, expFactor * exponentialScroll);
+            mouseScroll(currentScroll * expFactor);
+            timeSinceDecay = -1 / 30f;
 
-            float r = 4f * Input.GetAxis("Mouse ScrollWheel");
-            mainCamera.transform.Rotate(r, 0, 0);
-
-            if (mainCamera.transform.localRotation.x > upRotation || mainCamera.transform.localRotation.x < lowRotation)
-            {
-                mainCamera.transform.Rotate(-r, 0, 0);
-            }
         }
         else
         {
-            expFactor = 1;
+            timeSinceDecay += Time.deltaTime;
+            if (timeSinceDecay < (1 / 70f)) return;
+            timeSinceDecay = 0;
+
+            expFactor = expFactor * exponentialDecay;
+            if (expFactor > 1.0f)
+            {
+                mouseScroll(oldScroll * expFactor);
+            }
+            else
+            {
+                expFactor = 1;
+                oldScroll = 0;
+            }
         }
+    }
+    private void mouseScroll(float r) {
+        r = r * (float)0.25 * camSpeed;
+        tryTranslate(0, 15f * expFactor  * r, 0);
+        mainCamera.transform.Rotate(100 * r, 0, 0);
+        
+        if (mainCamera.transform.localRotation.x > upRotationBoundary || mainCamera.transform.localRotation.x < lowRotationBoundary)
+        {
+            mainCamera.transform.Rotate(100 * -r, 0, 0);
+        }
+         
+
     }
     private bool boundaryCheck() {
         if( transform.position[0] > upX     ||
             transform.position[0] < lowX    ||
-            transform.position[1] > upY     ||
-            transform.position[1] < lowY    ||
+            transform.position[1] > upYBoundary     ||
+            transform.position[1] < lowYBoundary    ||
             transform.position[2] > upZ     ||
             transform.position[2] < lowZ    )
         { return false; }
@@ -99,9 +135,9 @@ public class CameraController : MonoBehaviour {
         {
 
             transform.Translate(-x,-y,-z);
-            return true;
+            return false;
         }
-        return false;
+        return true;
     }
 
 }
